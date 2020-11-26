fe39c05fa68fe694c045c0ce9c91f75bc1e2bce6
man nguyen <nguyenlyminhman@gmail.com>
2020-11-26 16:08:11 +0700

    Add new protected route

--- src/routes/protectRoutes.js
+++ src/routes/protectRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 let Auth = require('../authen/middleware/AuthMiddleware');
 const cors = require('cors')
 const OraController = require("../controller/OraController");
+const MySqlController = require("../controller/MysqlController");
+const PgController = require('../controller/PgController');
 const AuthController = require("../authen/controllers/AuthController");
 
 /**
@@ -18,6 +20,8 @@ let protectAPIs = (app) => {
 
   router.use(Auth.isAuth)
   router.get("/demo/get-protect", OraController.getTestOracle)
+  // router.get("/demo/get-protect", MySqlController.getTestMysql);
+  // router.get("/demo/get-protect", PgController.getTestPg)
   return app.use("/", router);
 }
 module.exports.init = protectAPIs;
